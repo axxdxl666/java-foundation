e5a0eb0124138fde8703ffb22865f83d9dd28320
LAPTOP-FI6K4BQH\lenovo <xyqhtr@outlook.com>
2023-01-07 00:44:03 +0800

    while循环

---
+++ While.java
@@ -0,0 +1,12 @@
+package 循环;
+
+public class While {
+ public static void main(String[]args){
+	 int i=5;
+	 while(i<10){
+		 System.out.println("x的值是"+i);
+		 i++;
+		 System.out.println("\n");
+	 }
+ }
+}
