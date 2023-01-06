78423158c1f184dd322c20532f31e34210afae44
LAPTOP-FI6K4BQH\lenovo <xyqhtr@outlook.com>
2023-01-07 00:39:11 +0800

    switchcase基本用法

---
+++ Switch.java
@@ -0,0 +1,22 @@
+package switchcase;
+
+public class Switch {
+   public static void main(String[]args){
+	   int x='3';
+	   switch(x){
+	   case'1':
+		   System.out.println("x=1");
+	    break;
+	   case'2':
+		   System.out.println("x=2");
+		break;
+	   case'3':
+		   System.out.println("x=3");
+		break;
+	   case'4':
+		   System.out.println("x=4");
+
+		
+	   }	   
+   }
+}
