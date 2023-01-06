3a84c99b9db5fed6e805d9872bede302554ddded
LAPTOP-FI6K4BQH\lenovo <xyqhtr@outlook.com>
2023-01-07 00:50:27 +0800

    ifelse条件语句

---
+++ IfElse.java
@@ -0,0 +1,14 @@
+package 条件语句;
+
+public class IfElse {
+   public static void main (String[]args){
+	   int x=5;
+	   if(x>10){
+		   System.out.println("x大于10");
+		   
+	   }else{
+		   System.out.println("x小于10");
+		   
+	   }
+   }
+}
