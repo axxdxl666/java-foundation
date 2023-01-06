d692a2d80851e80afac974faaeace77147537cf7
LAPTOP-FI6K4BQH\lenovo <xyqhtr@outlook.com>
2023-01-07 00:37:29 +0800

    java加减乘除

---
+++ Textjiajianchengchu.java
@@ -0,0 +1,21 @@
+package java¼Ó¼õ³Ë³ý;
+
+public class Textjiajianchengchu {
+public static void main(String[]args){
+	int a=1;
+	int b=2;
+	int c=3;
+	int d=4;
+	System.out.println("a+b="+(a+b));
+	System.out.println("c+d="+(c+d));
+	System.out.println("a*b="+(a*b));
+	System.out.println("c/d="+(c/d));
+	System.out.println("a++="+(a++));
+	System.out.println("a--="+(a--));
+	System.out.println("c--="+(c--));
+	System.out.println("--c="+(--c));
+	
+	
+	
+}
+}
