2ac85acfc3ac7bad2d614617b22d20413d53758a
LAPTOP-FI6K4BQH\lenovo <xyqhtr@outlook.com>
2023-01-07 00:46:32 +0800

    dowhile循环

---
+++ DoWhile.java
@@ -0,0 +1,12 @@
+package 循环;
+
+public class DoWhile {
+	public static void main(String[]args){
+		int x=5;
+		do{
+			System.out.println("x的值是"+x);
+			x++;
+			System.out.println("\n");
+		}while(x<10);
+	}
+}
